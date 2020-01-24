1bae5deb27858de7799346085431a5d9ce8ab79b
Nino van Hooff <ninovanhooff@gmail.com>
2020-01-24 17:42:09 +0100

    Dox 2 Rst: Replace all function comments and fix file writing

--- dox_2_rst.py
+++ dox_2_rst.py
@@ -1,36 +1,43 @@
+import os
 import re
 
-class Dox2Rst:
 
-    REGEX = re.compile(r"(?P<before>[\s\S]*\n)?(?P<dox>\s*##.*\n(?:\s*#.*)*\n)(?P<def>\s*def.*)(?P<after>[\s\S]*)")
+class Dox2Rst:
+    REGEX = re.compile(r"(?P<before>[\s\S]*\n)?(?P<dox>\s*##.*\n(?:\s*#.*)*\n)(?P<def>\s*def.*)\n(?P<after>[\s\S]*)")
 
     COMMENT_INDENT_PATTERN = re.compile(r"^\s*#", re.MULTILINE)
-    COMMENT_INDENT_SUB = "  \g<0>"
+    COMMENT_INDENT_SUB = "    \g<0>"
 
     INDENT_PATTERN = re.compile(r"\s*")
     DOX_CONTINUATION_PREFIX_PATTERN = re.compile(r"( +#) *", re.MULTILINE)
 
-
-    def convert(self):
-
-        with open("plugins/CuraProfileWriter/CuraProfileWriter.py", "r+") as f:
+    def convert(self, file_path:str):
+        contents, changed = "", False
+        with open(file_path, "r") as f:
             contents = f.read()
-            match = self.REGEX.match(contents)
-            if match is not None:
-                comment_block = match.group("dox")
-                comment_block = self.add_indent(comment_block)
-                comment_block = self.convert_comment_block(comment_block)
-                contents = "{before}{definition}\n{rst}{after}".format(
-                    before=match.group("before"),
-                    definition=match.group("def"),
-                    rst=comment_block,
-                    after=match.group("after")
-                )
-                f.truncate()
-                f.seek(0)
-                f.write(contents)
-            else:
-                print("not found")
+            changed = True
+            while changed:
+                contents, changed = self.replace_first_dox_comment(contents)
+
+        os.remove(file_path)
+        with open(file_path, "w+") as f:
+            f.write(contents)
+
+    def replace_first_dox_comment(self, contents: str):
+        match = self.REGEX.match(contents)
+        if match is not None:
+            comment_block = match.group("dox")
+            comment_block = self.add_indent(comment_block)
+            comment_block = self.convert_comment_block(comment_block)
+            contents = "{before}{definition}\n{rst}{after}".format(
+                before=match.group("before"),
+                definition=match.group("def"),
+                rst=comment_block,
+                after=match.group("after")
+            )
+            return contents, True
+        else:
+            return contents, False
 
     def convert_comment_block(self, dox_block: str):
         """
@@ -53,15 +60,12 @@ class Dox2Rst:
         output = output.replace(":endcode", "")
         output = output.replace(":return", ":return:")
         # Add closing """
-        output = "{before}\"\"\"\n{indent}".format(before=output, indent=indent)
+        output = "{before}{indent}\"\"\"\n".format(before=output, indent=indent)
         return output
 
     def add_indent(self, comment_block: str):
         return re.sub(self.COMMENT_INDENT_PATTERN, self.COMMENT_INDENT_SUB, comment_block)
 
 
-
 if __name__ == "__main__":
-    Dox2Rst().convert()
-
-
+    Dox2Rst().convert("plugins/FirmwareUpdateChecker/FirmwareUpdateChecker.py")
